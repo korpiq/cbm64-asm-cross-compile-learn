b728bbd3602880c74a7de080b293d50e2bfbd3f6
Kalle Hallivuori <kato@iki.fi>
2025-09-15 20:02:51 +0300

    specify start address in asm for prg header

--- asm/korpiq-1.asm
+++ asm/korpiq-1.asm
@@ -1,9 +1,12 @@
 ; korpiq-1.asm
-;.feature pc_assignment
-;*=$0801
+.feature pc_assignment
+
 
 .export start
 
+*=$0801
+.word * ; first two bytes of a PRG file: starting memory address to load rest of the file at
+*=$0801
 start:
     ldy #$00
 loop1:
