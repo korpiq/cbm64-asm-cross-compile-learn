a89c79bf1c929429fc6453d102e838250aecf483
Kalle Hallivuori <kato@iki.fi>
2025-09-22 15:41:45 +0300

    build script to use build dir

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-ca65 -t c64 -l asm/korpiq-1.lst asm/korpiq-1.asm
-ld65 -C ld65/c64.cfg -o korpiq-1.prg asm/korpiq-1.o -m asm/korpiq-1.map
-c1541 -format korpiq-disk,01 d64 korpiq-disk.d64 -attach korpiq-disk.d64 -write korpiq-1.prg korpiq-1
+mkdir -p build
+ca65 -t c64 -l build/korpiq-1.lst -o build/korpiq-1.o asm/korpiq-1.asm
+ld65 -C ld65/c64.cfg -o build/korpiq-1.prg build/korpiq-1.o -m build/korpiq-1.map
+c1541 -format korpiq-disk,01 d64 korpiq-disk.d64 -attach korpiq-disk.d64 -write build/korpiq-1.prg korpiq-1
--- compile.sh
+++
@@ -1,2 +0,0 @@
-ca65 -t c64 -l asm/korpiq-1.lst asm/korpiq-1.asm
-ld65 -C ld65/c64.cfg -o korpiq-1.prg asm/korpiq-1.o -m asm/korpiq-1.map
